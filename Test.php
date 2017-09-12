d0708433261bb61aca24124f1c6b18ff919a29f1
MisssYang <1722318623@qq.com>
2017-09-12 16:49:04 +0800

    first

---
+++ Test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tinywan
+ * Date: 2017/9/12
+ * Time: 16:30
+ * Mail: Overcome.wan@Gmail.com
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+use LuaPackagist\Hello;
+echo Hello::Test();
---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Tinywan
+ * Date: 2017/9/12
+ * Time: 16:24
+ * Mail: Overcome.wan@Gmail.com
+ */
+namespace LuaPackagist;
+
+class Hello
+{
+    public static function Test()
+    {
+        return 'Hello Tinywan';
+    }
+}
